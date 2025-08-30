37eb23fac3c600774e8dda8b8c7e21085d3a221d
Aaron Mercado <aaron.dev2898@gmail.com>
2025-08-30 18:05:08 +0800

    commit push

--- app/src/main/java/com/example/smartai_basedfiremonitoring/Fragments/UserFragments/ProfileFragment.java
+++ app/src/main/java/com/example/smartai_basedfiremonitoring/Fragments/UserFragments/ProfileFragment.java
@@ -7,16 +7,58 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.smartai_basedfiremonitoring.Model.User;
 import com.example.smartai_basedfiremonitoring.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class ProfileFragment extends Fragment {
 
+    private EditText name, email, password;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Profile(view);
+        return view;
+    }
+
+    public void Profile(View view){
+        name = view.findViewById(R.id.name);
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
+        if (currentUser != null){
+            String userId = currentUser.getUid();
+            databaseReference.child(userId).get().addOnSuccessListener(snapShot -> {
+                User user = snapShot.getValue(User.class);
+
+                if (user != null){
+                    name.setText(user.getUsername());
+                    email.setText(user.getEmail());
+                }
+
+            }).addOnFailureListener(e -> {
+                Toast.makeText(getContext(), "Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            });
+
+
+
+
+        }
+
+
+
     }
 }
